e2f47d017cef8ecb2a566458ac3852b0cf0f3f9d
JoelHJames <Joel.Hernandez-James@com.state.oh.us>
2020-01-08 10:46:32 -0500

    fixed bug  with serverfile

--- server.js
+++ server.js
@@ -5,22 +5,24 @@ const request = require('request');
 const app = express()
 const apiKey = '2019f6e90de49794bd4f188e99f1d6a5';
 
-app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
 
 
 // GET
 app.get('/', function (req, res){
-   res.render('index');
+    res.render('index', {weather: null, error: null});
 })
 
 
 // POST
 app.post('/', function (req, res) {
+   
     let city = req.body.city;
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-  request(url, function (err, response, body) {
+ 
+    request(url, function (err, response, body) {
       if(err){
         res.render('index', {weather: null, error: 'Error, please try again'});
       } else {
